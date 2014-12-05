a7396a86327d92698e59a69746aec431f5be1151
HZ-8 <h.zaletska@gmail.com>
2014-12-05 11:32:12 +0200

    Amount of universal strings calculation

--- Chapter 4/Pass_Test.py
+++ Chapter 4/Pass_Test.py
@@ -6,3 +6,27 @@ k = int(k)
 result = StringComposition.StringComposition(Text, k)
 
 WriteArrayToFile.WriteArrayToFile(result)
+
+'''bin4 = lambda x : ''.join(reversed( [str((x >> i) & 1) for i in range(4)]))
+bin19 = lambda x : ''.join(reversed( [str((x >> i) & 1) for i in range(19)]))
+
+dic = []
+for i in range(2 ** 4):    
+    s = bin4(i)
+    dic.append(s)
+
+dic.sort()
+print dic 
+
+count = 0
+for i in range(2 ** 19):    
+    s = bin19(i)
+    
+    temp = []    
+    for j in range(16):
+        temp.append(s[j:j+4])
+    temp.sort()
+    
+    if dic == temp:
+        count +=1
+print count''''
